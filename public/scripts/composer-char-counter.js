52a6867a4fb4ec8c890cc6baed14c0af93d08a15
Alex Thon <ghaiquol@gmail.com>
2021-11-18 18:55:54 +0000

    Add content to composer-char-counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,29 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
 
 $(document).ready(() => {
-  $('#tweet-text').on('focus', function() {
+  // eslint-disable-next-line no-undef
+  $('#tweet-text').on('keydown', function() {
     
-    console.log(this);
+    // keydown event for textarea
+    // count the length of characters typed
+    let $tweets = this.value.length;
+  
+    const $currenTweetsNumber = $('.counter');
+    const $charactersLeft = 140 - $tweets;
+
+    // update the counter value. Change color for negative values
+    // but retain the original color for positive values
+    $charactersLeft > -1 ? $currenTweetsNumber.text($charactersLeft) : $currenTweetsNumber.text($charactersLeft).css("color", "red");
+
+    
+    
+   
+    
+    
+    
+    
+    
 
   });
 });
