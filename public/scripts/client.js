edbeaa7c41cdc7090e9834144ea71c99c7871b97
Alex Thon <ghaiquol@gmail.com>
2021-11-19 20:40:01 +0000

    Add responsive design and error-message box

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,56 +8,32 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 
-
-const renderTweets = function(tweets) {
+$(document).ready(function() {
+  $('.error-message').hide();
+  const renderTweets = function(tweets) {
   // loops through tweets
-  for (let tweet of tweets) {
-    const $tweet = createTweetElement(tweet);
-    $('.tweets-outer-container').append($tweet);
-  }
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-};
-// helper function that provides time
-const time = (obj) => timeago.format(obj.created_at);
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('.tweets-outer-container').prepend($tweet);
+    }
+  };
+  // helper function that provides time
+  
 
 
-const createTweetElement = (tweetObj) => {
-  return (
-    `<article class="tweet-container">
+  const createTweetElement = (tweetObj) => {
+    const time = (obj) => timeago.format(obj.created_at);
+    return (
+      `<article class="tweet-container">
             <section class="tweet-header">
               <img src=${tweetObj.user.avatars}><span>${tweetObj.user.name}</span>
               <div class="handle">${tweetObj.user.handle}</div>
             </section>
             <p>${tweetObj.content.text}</p>
             <div class="line"></div>
-            <section class="days-container"><span>${time(tweetObj.created_at)}</span>
+            <section class="days-container"><span>${timeago.format(new Date())}</span>
               <div class="icons-container">
                 <i class="fas fa-flag"></i>
                 <i class="fas fa-retweet"></i>
@@ -65,62 +41,53 @@ const createTweetElement = (tweetObj) => {
               </div>
             </section>
     </article>`
-  );
-};
+    );
+  };
 
-
-// $('#tweet-text').on('keydown', function() {
-    
-//   // keydown event for textarea
-//   // count the length of characters typed
-//   let $tweets = this.value.length;
-
-//   const $currenTweetsNumber = $('.counter');
-//   const $charactersLeft = 140 - $tweets;
-
-//   // update the counter value. Change color for negative values
-//   // but retain the original color for positive values
-//   $charactersLeft > -1 ? $currenTweetsNumber.text($charactersLeft) : $currenTweetsNumber.text($charactersLeft).css("color", "red");
-
-// });
-
-// $.ajax({
-//   type: "POST",
-//   url: url,
-//   data: data,
-//   success: success,
-//   dataType: dataType
-// });
-
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-//$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
-$(document).ready(function() {
-  renderTweets(data);
   // add an event listener that listens for the submit event
   $('form').on('submit', function(evt) {
     // prevent the default behaviour of the submit event (data submission and page refresh)
     evt.preventDefault();
+    
+    const $data = $(this).serialize();
 
-  
-    $.ajax({
-      type: "POST",
-      url: '/tweets/',
-      data: {$('#tweet-text').val()},
-      contentType: "application/json; charset=utf-8",
-      dataType: "json",
-      encode: true,
-    }).done(data => console.log(data));
+    const $content = $('#tweet-text');
+    const $contentVal = $content.val();
+    
+
+
+    if ($contentVal === "" || $contentVal === null) {
+      $('.error-message').text("Add some content");
+      $('.error-message').show();
+
+    } else if (evt.target[0].value.length > 140) {
+      $('.error-message').show();
+      $('.error-message').text("Too many characters typed!");
       
-   
+    } else {
+     
+      $.post('/tweets', $data).done((response)=> {
+        console.log("response is: ", response);
+        $('textarea').val('');
+        $('.counter').text(140);
+        $('.tweets-outer-container').empty();
+        loadTweets();
+      });
+    }
+
   });
+  
+  const loadTweets = () => {
+    $('.error-message').hide();
+    $.get('/tweets').done(data => {
+      console.log("sucess: ", data);
+      renderTweets(data);
+    });
+  };
+
+  loadTweets();
+
 });
 
 
-// }).done(function (data) {
-  //       console.log(data);
-  //     });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -8,13 +8,13 @@ $(document).ready(() => {
     // keydown event for textarea
     // count the length of characters typed
     let $tweets = this.value.length;
-  
+    
     const $currenTweetsNumber = $('.counter');
     const $charactersLeft = 140 - $tweets;
 
     // update the counter value. Change color for negative values
     // but retain the original color for positive values
-    $charactersLeft > -1 ? $currenTweetsNumber.text($charactersLeft) : $currenTweetsNumber.text($charactersLeft).css("color", "red");
+    $charactersLeft > -1 ? $currenTweetsNumber.text($charactersLeft).css("color", "grey") : $currenTweetsNumber.text($charactersLeft).css("color", "red");
   
   });
 });
